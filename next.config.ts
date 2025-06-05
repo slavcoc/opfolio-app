582cf4abb299d8c78d17e0c7181fb010ca990519
Slavcho Cvetanovski <cvetanovski.slavco@gmail.com>
2025-06-05 20:57:33 +0200

    fix images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  basePath: process.env.PAGES_BASE_PATH,
+  basePath:isProduction ? '/opfolio-app' : '',
   output: 'export',
   distDir: 'build',
   images: {
